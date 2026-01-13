c4a7d335edad32699726a53d56621c7a1e013270
Dorthy-d695 <dorthy695@gmail.com>
2026-01-13 18:22:30 +0530

    Added LeftTriangle.java

---
+++ patterns/basics/Left_aligned/LeftTriangle.java
@@ -0,0 +1,22 @@
+// Program to print a solid rectangle pattern
+// Input: n = 5
+// Output:
+// *
+// **
+// ***
+// ****
+// *****
+
+package Left_aligned;
+
+public class LeftTriangle {
+    public static void main(String[] args){
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
