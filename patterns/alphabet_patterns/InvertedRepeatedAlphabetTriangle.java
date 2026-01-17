3f4f1ed44cbd0b3e011687fc130441cbfe3fcbe8
Dorthy-d695 <dorthy695@gmail.com>
2026-01-17 17:44:43 +0530

    Added InvertedRepeatedTriangle.java

---
+++ patterns/alphabet_patterns/InvertedRepeatedAlphabetTriangle.java
@@ -0,0 +1,24 @@
+// Program to print a Inverted Repeated Alphabet Triangle pattern
+// Input: n = 5
+// Output:
+// A 
+// B B
+// C C C
+// D D D D
+// E E E E E
+
+package patterns.alphabet_patterns;
+
+public class InvertedRepeatedAlphabetTriangle {
+    public static void main(String[] args) {
+        int n=5,k=0;
+        for(int i=n;i>=1;i--){
+            char ch=(char)('A'+k);
+            for(int j=1;j<=i;j++){
+                System.out.print(ch+" ");
+            }
+            System.out.println();
+            k++;
+        }
+    }
+}
