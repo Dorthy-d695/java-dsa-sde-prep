65aeea6b903013329c56db37a94795c5ce3e1454
Dorthy-d695 <dorthy695@gmail.com>
2026-01-22 13:01:17 +0530

    Added SolidSquare.java

---
+++ patterns/basics/SolidSquare.java
@@ -0,0 +1,20 @@
+// Program to print a solid square pattern
+// Input: n = 5
+// Output:
+// ****
+// ****
+// ****
+// ****
+// ****
+
+public class SolidSquare{
+    public static void main(String[] args){
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
